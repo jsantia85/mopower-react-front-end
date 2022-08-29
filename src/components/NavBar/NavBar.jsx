67698c9dc654725f617658b16a7ab77c1d49f7e6
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-29 16:05:50 -0400

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,18 +8,24 @@ const NavBar = ({ user, handleLogout }) => {
       <nav class="navbar navbar-dark bg-dark fixed-top">
         {/* container for home btn and navBar toggle */}
         <div class="container-fluid">
-          <NavLink to='/' className={styles.link}>Home</NavLink>
+          <NavLink to='/' className={styles.link}>
+            Home
+          </NavLink>
           {/* code for navBar toggle */}
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
             <span class="navbar-toggler-icon"></span>
           </button>
           {/* code allows horizontal tab to open */}
           <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-
-          </div>
-        </div>
-      </nav>
-      {/* {user ?
+            {/* Code creates header for tab and adds close btn */}
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                Menu
+              </h5>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+            {user ?
         <nav>
           <ul>
             <li>Welcome, {user.name}</li>
@@ -35,7 +41,11 @@ const NavBar = ({ user, handleLogout }) => {
             <li><Link to="/signup">Sign Up</Link></li>
           </ul>
         </nav>
-      } */}
+      }
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
